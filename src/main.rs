c64bfec86bc3bf83b6f0f243912dfffd7cf45ce9
Anett Seeker <noeddl@users.noreply.github.com>
2019-09-07 21:12:20 +0200

    Let structopt directly parse Chord

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use std::str::FromStr;
 use structopt::StructOpt;
 use ukebox::chord::Chord;
 use ukebox::ukulele::Ukulele;
 
 #[derive(StructOpt)]
 struct Cmd {
-    chord: String,
+    chord: Chord,
 }
 
 fn main() {
     let args = Cmd::from_args();
     let mut uke = Ukulele::new();
-    let chord = Chord::from_str(&args.chord).unwrap();
-    uke.play(&chord, 0);
+    uke.play(&args.chord, 0);
 
-    println!("[{}]\n", chord);
+    println!("[{}]\n", args.chord);
     println!("{}", uke);
 }
