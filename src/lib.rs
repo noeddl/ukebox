5bafd7c0b698b423850569f5d10c67df6238bbc2
Anett Seeker <noeddl@users.noreply.github.com>
2019-09-10 22:57:47 +0200

    Allow for negative values of type Frets

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,7 @@ mod streng;
 pub mod ukulele;
 
 /// Type for the number of frets (corresponds to the number of semitones)
-/// to move from one note or pitch class to another.
-type Frets = u8;
+/// to move from one note or pitch class to another. It can take positive
+/// and negative values as you can move up and down the fretboard (or the
+/// scale).
+type Frets = i8;
