b7df1c6c72ed7dcdf3a15a68b2d047b0591d024c
Anett Seeker <noeddl@users.noreply.github.com>
2019-09-17 21:26:35 +0200

    Add enum StaffPosition

--- src/note/mod.rs
+++ src/note/mod.rs
@@ -1,6 +1,8 @@
 #![allow(clippy::module_inception)]
 mod note;
 mod pitch_class;
+mod staff_position;
 
 pub use self::note::Note;
 pub use self::pitch_class::PitchClass;
+pub use self::staff_position::StaffPosition;
---
+++ src/note/staff_position.rs
@@ -0,0 +1,92 @@
+use std::ops::Add;
+
+const STAFF_POSITION_COUNT: u8 = 7;
+
+/// The vertical position of the notehead on the staff (on a line or in a space).
+/// We use the staff position of an enharmonic note to decide whether it is sharp
+/// or flat.
+///
+/// For example, a note with pitch class `FSharp` can either be written as `F#`
+/// (if its staff position is `FPos` or `Gb` (if its staff position is `GPos`).
+///
+/// https://en.wikipedia.org/wiki/Staff_(music)#Staff_positions
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum StaffPosition {
+    CPos,
+    DPos,
+    EPos,
+    FPos,
+    GPos,
+    APos,
+    BPos,
+}
+
+impl From<u8> for StaffPosition {
+    fn from(n: u8) -> Self {
+        use StaffPosition::*;
+
+        // Make sure we get a value between 0 and 6.
+        let v = n % STAFF_POSITION_COUNT;
+
+        // There does not seem to be a good way to turn integers into enum
+        // variants without using external crates. Hardcoding the mapping
+        // is not so elegant but at least readable.
+        match v {
+            0 => CPos,
+            1 => DPos,
+            2 => EPos,
+            3 => FPos,
+            4 => GPos,
+            5 => APos,
+            6 => BPos,
+            // Because of the modulo, `v` will always be in the correct range.
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Add<u8> for StaffPosition {
+    type Output = Self;
+
+    /// Get the staff position that is `n` positions higher than the current one.
+    fn add(self, n: u8) -> Self {
+        Self::from(self as u8 + n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest_parametrize;
+    use StaffPosition::*;
+
+    #[rstest_parametrize(
+        n,
+        staff_position,
+        case(0, CPos),
+        case(1, DPos),
+        case(2, EPos),
+        case(3, FPos),
+        case(4, GPos),
+        case(5, APos),
+        case(6, BPos)
+    )]
+    fn test_from_int(n: u8, staff_position: StaffPosition) {
+        assert_eq!(StaffPosition::from(n), staff_position);
+    }
+
+    #[rstest_parametrize(
+        staff_position,
+        n,
+        result,
+        case(CPos, 0, CPos),
+        case(CPos, 1, DPos),
+        case(DPos, 2, FPos),
+        case(CPos, 7, CPos),
+        case(CPos, 8, DPos),
+        case(CPos, 14, CPos)
+    )]
+    fn test_pitch_class_add_int(staff_position: StaffPosition, n: u8, result: StaffPosition) {
+        assert_eq!(staff_position + n, result);
+    }
+}
