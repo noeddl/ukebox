647811ec5729835309e2005dc4cb66a46adbcfb1
Anett Seeker <noeddl@users.noreply.github.com>
2019-09-17 21:34:44 +0200

    Add enum Interval

--- src/note/mod.rs
+++ src/note/mod.rs
@@ -1,8 +1,10 @@
 #![allow(clippy::module_inception)]
+mod interval;
 mod note;
 mod pitch_class;
 mod staff_position;
 
+pub use self::interval::Interval;
 pub use self::note::Note;
 pub use self::pitch_class::PitchClass;
 pub use self::staff_position::StaffPosition;
