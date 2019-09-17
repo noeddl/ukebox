57049a344ae093a04d76c3717d4c966d11dd4a52
Anett Seeker <noeddl@users.noreply.github.com>
2019-09-17 10:28:14 +0200

    Move ChordShape and ChordShapeSet to new module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod chord;
+mod chord_shape;
 mod note;
 mod streng;
 pub mod ukulele;
@@ -6,3 +7,6 @@ pub mod ukulele;
 /// Type for the number of frets (corresponds to the number of semitones)
 /// to move from one note or pitch class to another.
 pub type Frets = u8;
+
+/// Number of strings on our string instrument.
+pub const STRING_COUNT: usize = 4;
