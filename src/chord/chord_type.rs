352861355daeae1194718f28f596452c8fd1efad
Anett Seeker <noeddl@users.noreply.github.com>
2019-12-27 17:12:03 +0100

    Determine the chord type from a list of pitch classes

--- src/chord/chord_type.rs
+++ src/chord/chord_type.rs
@@ -1,4 +1,6 @@
 use crate::note::Interval;
+use crate::note::PitchClass;
+use std::convert::TryFrom;
 use std::fmt;
 use std::str::FromStr;
 
@@ -71,5 +73,76 @@ impl fmt::Display for ChordType {
         };
 
         write!(f, "{}", s)
+    }
+}
+
+impl TryFrom<&[PitchClass]> for ChordType {
+    type Error = &'static str;
+
+    /// Determine the chord type from a list of pitch classes representing a chord.
+    fn try_from(pitches: &[PitchClass]) -> Result<Self, Self::Error> {
+        use ChordType::*;
+
+        // Subtract the root note's pitch class from all pitch classes to get the
+        // difference in semitones.
+        let mut pitch_diffs: Vec<_> = pitches.iter().map(|pc| *pc - pitches[0]).collect();
+
+        pitch_diffs.sort();
+
+        match pitch_diffs[..] {
+            [0, 4, 7] => Ok(Major),
+            [0, 3, 7] => Ok(Minor),
+            [0, 2, 7] => Ok(SuspendedSecond),
+            [0, 5, 7] => Ok(SuspendedFourth),
+            [0, 4, 8] => Ok(Augmented),
+            [0, 3, 6] => Ok(Diminished),
+            [0, 4, 7, 10] => Ok(DominantSeventh),
+            [0, 3, 7, 10] => Ok(MinorSeventh),
+            [0, 4, 7, 11] => Ok(MajorSeventh),
+            [0, 3, 7, 11] => Ok(MinorMajorSeventh),
+            [0, 4, 8, 10] => Ok(AugmentedSeventh),
+            [0, 4, 8, 11] => Ok(AugmentedMajorSeventh),
+            [0, 3, 6, 9] => Ok(DiminishedSeventh),
+            [0, 3, 6, 10] => Ok(HalfDiminishedSeventh),
+            _ => Err("No matching chord type found."),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(clippy::many_single_char_names)]
+    use super::*;
+    use rstest::rstest;
+    use ChordType::*;
+    use PitchClass::*;
+
+    #[rstest(
+        pitches, chord_type,
+        // Test C-chords.
+        case(vec![C, E, G], Major),
+        case(vec![C, DSharp, G], Minor),
+        case(vec![C, D, G], SuspendedSecond),
+        case(vec![C, F, G], SuspendedFourth),
+        case(vec![C, E, GSharp], Augmented),
+        case(vec![C, DSharp, FSharp], Diminished),
+        case(vec![C, E, G, ASharp], DominantSeventh),
+        case(vec![C, DSharp, G, ASharp], MinorSeventh),
+        case(vec![C, E, G, B], MajorSeventh),
+        case(vec![C, DSharp, G, B], MinorMajorSeventh),
+        case(vec![C, E, GSharp, ASharp], AugmentedSeventh),
+        case(vec![C, E, GSharp, B], AugmentedMajorSeventh),
+        case(vec![C, DSharp, FSharp, A], DiminishedSeventh),
+        case(vec![C, DSharp, FSharp, ASharp], HalfDiminishedSeventh),
+        // Test some chords with other root notes.
+        case(vec![D, FSharp, A], Major),
+        case(vec![D, F, A], Minor),
+        case(vec![D, FSharp, A, C], DominantSeventh),
+        case(vec![G, B, D], Major),
+        // Test pitch class list in different order.
+        case(vec![C, G, E], Major),
+    )]
+    fn test_get_chord_type(pitches: Vec<PitchClass>, chord_type: ChordType) {
+        assert_eq!(ChordType::try_from(&pitches[..]).unwrap(), chord_type);
     }
 }
