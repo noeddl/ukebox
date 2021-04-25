b5f0a8b187a7a5cafe10967c7f32873eaddd4491
Anett Seeker <noeddl@users.noreply.github.com>
2021-04-25 22:53:11 +0200

    Implement distance between two fingerings

--- src/fingering.rs
+++ src/fingering.rs
@@ -1,8 +1,34 @@
+use std::cmp::{max, min};
+
 use crate::{FingerPosition, Voicing, FINGER_COUNT};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Fingering {
     finger_positions: [FingerPosition; FINGER_COUNT],
+}
+
+impl Fingering {
+    pub fn distance(&self, other: Fingering) -> u8 {
+        let dist = |(&(s1, f1), &(s2, f2))| {
+            let add = s1 == 0 && s2 != 0;
+            let remove = s1 != 0 && s2 == 0;
+            let slide = s1 == s2 && f1 != f2;
+
+            // Simple finger movements.
+            if add || remove || slide {
+                1
+            // Move finger from one string to another.
+            } else {
+                (max(s1, s2) - min(s1, s2)) + (max(f1, f2) - min(f1, f2))
+            }
+        };
+
+        self.finger_positions
+            .iter()
+            .zip(other.finger_positions.iter())
+            .map(dist)
+            .sum()
+    }
 }
 
 impl From<[FingerPosition; FINGER_COUNT]> for Fingering {
@@ -55,5 +81,23 @@ mod tests {
         let fingering1 = Fingering::from(voicing);
         let fingering2 = Fingering::from(finger_positions);
         assert_eq!(fingering1, fingering2);
+    }
+
+    #[rstest(
+        finger_positions1, finger_positions2, distance,
+        case([(0, 0), (0, 0), (0, 0), (0, 0)], [(0, 0), (0, 0), (0, 0), (0, 0)], 0),
+        case([(1, 2), (2, 2), (3, 2), (0, 0)], [(1, 2), (2, 2), (3, 2), (0, 0)], 0),
+        case([(0, 0), (0, 0), (0, 0), (0, 0)], [(1, 2), (2, 2), (3, 2), (0, 0)], 3),
+        case([(1, 2), (2, 2), (3, 2), (0, 0)], [(0, 0), (0, 0), (0, 0), (0, 0)], 3),
+        case([(1, 2), (2, 2), (3, 2), (0, 0)], [(1, 2), (2, 2), (3, 3), (0, 0)], 1),
+    )]
+    fn test_distance(
+        finger_positions1: [FingerPosition; FINGER_COUNT],
+        finger_positions2: [FingerPosition; FINGER_COUNT],
+        distance: u8,
+    ) {
+        let fingering1 = Fingering::from(finger_positions1);
+        let fingering2 = Fingering::from(finger_positions2);
+        assert_eq!(fingering1.distance(fingering2), distance);
     }
 }
